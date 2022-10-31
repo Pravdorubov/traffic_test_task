486f09528792f78abc5f874c3149ba5a887032ec
Leshukov Viktor <leshukov.broccoligames@gmail.com>
2022-10-31 13:26:17 +0300

    minor refactoring

--- traffic_test_task/urls.py
+++ traffic_test_task/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from ttt import views
 
 urlpatterns = [
-    path('ttt/', include('ttt.urls')),
+    path('', views.index, name='index'),
+    # path('ttt/', include('ttt.urls')),
     path('admin/', admin.site.urls),
 ]
